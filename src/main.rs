3d2e9fb241dc8907d92de63bb781bf6da2598104
Liam Avella-Pisera <50953884+paperclipmaximizer@users.noreply.github.com>
2024-04-11 17:42:34 +1000

    improve base project structure
    * add actions for db config
    * add better file structure, 
    * minor refactor, 
    * improved coverage, 
    * routing template work
    * db testing schema
    * config reader
    * dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::net::TcpListener;
-
-use jiro::run;
+use jiro::start_server::run;
+use jiro::configuration::get_configuration;
+use sqlx::postgres::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
